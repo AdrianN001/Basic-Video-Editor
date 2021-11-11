c4859b8c8c380e030d3d949a85f72ff1cc201422
AdrianN001 <gogos.adrian2006@gmail.com>
2021-11-11 17:36:29 +0100

    The file choosing is done. No mp4 player yet

--- 3.py
+++ 3.py
@@ -1,15 +1,27 @@
 from tkinter import *
-import tkinter
+from tkinter import filedialog
+import tkinter as tkr
 import moviepy
 from moviepy.editor import *
+import os 
+from tkinter import *
+from PIL import ImageTk,Image
 
 
-root = tkinter.Tk()
+root = tkr.Tk()
 
 root.title('Video Editor')
+root.geometry('500x400')
 
 
+greeting = tkr.Label(master = root,text= "Welcom To My Video Editor").pack()
 
+def openingAFile():
+    root.filenames = filedialog.askopenfilenames(initialdir='/', title = 'Choose a file', filetypes=(("MP4 files", "*.mp4"),("all files", "*.*")))
+    image = ImageTk.PhotoImage(Image.open(root.filenames[0]))
+    imageLabel = tkr.Laber(image = image).pack()
+
+chooseingFile = tkr.Button(master = root,text = "Choose a File", command= openingAFile).pack()
 
 
 
