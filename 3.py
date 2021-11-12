cba67a5be86401eafefdcecf2e9296d11ca03f6f
AdrianN001 <gogos.adrian2006@gmail.com>
2021-11-12 17:00:05 +0100

    Day 3, it is actually working, need for a few updates

--- 3.py
+++ 3.py
@@ -4,8 +4,9 @@ import tkinter as tkr
 import moviepy
 from moviepy.editor import *
 import os 
-from tkinter import *
 from PIL import ImageTk,Image
+import time
+
 
 
 root = tkr.Tk()
@@ -14,14 +15,28 @@ root.title('Video Editor')
 root.geometry('500x400')
 
 
+def calc():
+    file1 = filedialog.askopenfilename(initialdir='/', title = 'Choose a file', filetypes=(("MP4 file", "*.mp4"),("all files", "*.*")))
+    tkr.Label(master = root,text= file1).pack()
+
+
+    if file1 != None:
+        kezdet = tkr.Entry(master = root)
+        kezdet.pack()
+        veg = tkr.Entry(master = root)
+        veg.pack()
+        
+        def videoEdit():
+            clip2 = VideoFileClip(file1).subclip(kezdet.get(),veg.get())
+            clip2.write_videofile("Final_Video.mp4") 
+        submit = tkr.Button(text='Submit', command=videoEdit)
+        submit.pack()
+    
+
+
+
 greeting = tkr.Label(master = root,text= "Welcom To My Video Editor").pack()
-
-def openingAFile():
-    root.filenames = filedialog.askopenfilenames(initialdir='/', title = 'Choose a file', filetypes=(("MP4 files", "*.mp4"),("all files", "*.*")))
-    image = ImageTk.PhotoImage(Image.open(root.filenames[0]))
-    imageLabel = tkr.Laber(image = image).pack()
-
-chooseingFile = tkr.Button(master = root,text = "Choose a File", command= openingAFile).pack()
+chooseingFile = tkr.Button(master = root,text = "Choose a File", command= calc).pack()
 
 
 
